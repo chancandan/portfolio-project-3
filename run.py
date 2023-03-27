7145f10ed4c026e8d7c0adbef02cef4dbaea20a7
chancandan <daniellyons@live.ie>
2023-03-27 17:51:26 +0000

    Code random module to select a word from word list

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 import random
 
 def game_word(difficulty):
-    
+    if difficulty == "easy":
+        word_list = ["cat", "dog", "hat", "car", "tree", "book", "door", "cake", "bird", "pen"]
+    elif difficulty == "medium":
+        word_list = ["computer", "python", "jacket", "sunflower", "television", "guitar", "popcorn", "giraffe", "restaurant", "hamster"]
+    elif difficulty == "hard":
+        word_list = ["xylophone", "juxtaposition", "quagmire", "mnemonic", "haphazard", "furtive", "belligerent", "ubiquitous", "zeitgeist", "quintessential"]
+    else:
+        print("That's not a valid difficulty level. Please choose from either easy, medium or hard.")
+    return None
+
+    word = random.choice(word_list)
+    return word
+
+
